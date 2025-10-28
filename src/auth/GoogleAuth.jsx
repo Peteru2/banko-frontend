1d0183a1a64cf2487c697baa280c41ebbe41fccd
Olatunji Peter <63746720+Peteru2@users.noreply.github.com>
2025-10-28 21:24:17 +0100

    google sign-n freeze issue on mobile fix in progress

--- src/auth/GoogleAuth.jsx
+++ src/auth/GoogleAuth.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, {useContext,useEffect} from 'react'
 import { GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from 'jwt-decode';
 import api from "../services/api"
@@ -17,34 +17,6 @@ const GoogleAuth = () => {
       console.log(res.data);
   };
 
-  useEffect(() => {
-    if (!window.google) return;
-    const container = document.getElementById("googleBtn");
-    container.innerHTML = "";
-
-    window.google.accounts.id.initialize({
-      client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
-      callback: handleSuccess,
-      auto_select: false,
-      cancel_on_tap_outside: false,
-    });
-    const isMobile = /Mobi|Android/i.test(navigator.userAgent);
-
-    if (isMobile) {
-      window.google.accounts.id.prompt((notification) => {
-        if (notification.isNotDisplayed()) {
-          console.warn("Google prompt not displayed on mobile");
-        }
-      });
-    } else {
-      window.google.accounts.id.renderButton(container, {
-        theme: "filled_black",
-        size: "large",
-        text: "continue_with",
-        shape: "pill",
-      });
-    }
-  }, []);
 
   const handleError = () => {
       console.log('Login Failed');
